7c86f7c6f76f51b62d22f470b614b0da1a9be711
DanLufar <mshevchenko1984@gmail.com>
2024-10-26 11:01:26 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,22 @@
+package app;
+
+public class Main {
+    private static final double CONV_K = 2.20462;
+
+    public static void main(String[] args) {
+        System.out.println("Convertor App.");
+
+        double kgs = 5;
+        double pounds = convKgsToPounds(kgs);
+        System.out.println("Resalt is " + pounds + " pounds.");
+    }
+
+    private static double convKgsToPounds(double kgs) {
+        return kgs * CONV_K;
+    }
+}
+
+
+
+
+
